1a2d5df94d2d2a2281f0ce219b6cea4f164f51b3
josh231101 <wiijosue333@gmail.com>
2020-07-14 01:01:48 -0500

    Cards from database add

--- app.js
+++ app.js
@@ -8,8 +8,38 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+mongoose.connect("mongodb+srv://admin-josue:Test123@cluster0.usnm4.mongodb.net/soups?retryWrites=true&w=majority", {useNewUrlParser : true})
+const soupSchema = mongoose.Schema({
+  imgUrl : {
+    type : String,
+    required : [1, "https://uss.com.ar/corporativo/wp-content/themes/consultix/images/no-image-found-360x260.png"]
+  },
+  title : String,
+  stars : {
+    type : Number,
+    min : 0,
+    max : 5
+  },
+  price : String
+})
+
+const Soup = new mongoose.model("Soup",soupSchema);
+
+const newSoup = new Soup({
+  imgUrl : "https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2016/08/caldoderes.jpg",
+  title : "Caldo de pollo",
+  stars : 4.5
+})
+
 app.get("/",function(req,res){
-  res.render("home")
+  Soup.find(function(err,foundsoups){
+    if(!err){
+      res.render("home",{
+        soups : foundsoups
+      })
+    }
+    else{console.log(err);}
+  })
 })
 
 app.listen(8080,function(){
