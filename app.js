30cc3baeb9df2cd78a6ca50fd4a40f65f4a332b1
josh231101 <wiijosue333@gmail.com>
2020-07-15 12:52:19 -0500

    Avoid cart repetition, todo fix product number bug

--- app.js
+++ app.js
@@ -7,38 +7,42 @@ const ejs = require("ejs");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD +"@cluster0.usnm4.mongodb.net/soups?retryWrites=true&w=majority", {useNewUrlParser : true})
+mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@cluster0.usnm4.mongodb.net/soups?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+})
 const soupSchema = mongoose.Schema({
-  imgUrl : {
-    type : String,
-    required : [1, "https://uss.com.ar/corporativo/wp-content/themes/consultix/images/no-image-found-360x260.png"]
+  imgUrl: {
+    type: String,
+    required: [1, "https://uss.com.ar/corporativo/wp-content/themes/consultix/images/no-image-found-360x260.png"]
   },
-  title : String,
-  stars : {
-    type : Number,
-    min : 0,
-    max : 5
+  title: String,
+  stars: {
+    type: Number,
+    min: 0,
+    max: 5
   },
-  price : Number
+  price: Number
 })
-const Soup = new mongoose.model("Soup",soupSchema);
-const userSchema =  mongoose.Schema({
-  name : String,
-  lastName : String,
-  address : String,
-  email : String,
-  password : String,
-  order : [soupSchema]
+const Soup = new mongoose.model("Soup", soupSchema);
+const userSchema = mongoose.Schema({
+  name: String,
+  lastName: String,
+  address: String,
+  email: String,
+  password: String,
+  order: [soupSchema]
 })
-const User = new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 const newuser = new User({
-  name : "Josue",
-  lastName : "Arreola",
-  address : "Palma Real 243",
-  email : "wiijosue333@gmail.com",
-  password : "12345"
+  name: "Josue",
+  lastName: "Arreola",
+  address: "Palma Real 243",
+  email: "wiijosue333@gmail.com",
+  password: "12345"
 })
 // newuser.save();
 
@@ -62,84 +66,129 @@ newSoup.save();*/
   })
 })*/
 
-app.get("/",function(req,res){
-  Soup.find({stars : { $gte : 4.4 }},function(err,foundsoups){
-    if(!err){
-      var selectedSoups = foundsoups.slice(0,4);
-      res.render("home",{
-        soups : selectedSoups
-      })
+app.get("/", function(req, res) {
+  Soup.find({
+    stars: {
+      $gte: 4.4
     }
-    else{console.log(err);}
+  }, function(err, foundsoups) {
+    if (!err) {
+      var selectedSoups = foundsoups.slice(0, 4);
+      res.render("home", {
+        soups: selectedSoups
+      })
+    } else {
+      console.log(err);
+    }
   })
 })
-app.get("/signin",function(req,res){
-  res.render("login",{
-    id : ""
+app.get("/signin", function(req, res) {
+  res.render("login", {
+    id: ""
   })
 })
-app.post("/signin", function(req,res){
+app.post("/signin", function(req, res) {
   const useremail = req.body.email;
   const userpassword = req.body.password;
   const soupId = req.body.id;
   console.log(soupId);
 
-  User.findOne({email : useremail},function(e,foundUser){
-    if(!e){
-      if(foundUser){
-        if(foundUser.password === userpassword){
-          if(soupId != null ){
-            Soup.findOne({_id : soupId},function(e,foundSoup){
-              if(foundSoup){
-
-                User.updateOne( {_id: foundUser._id},{$push : {order : foundSoup }},function(e){
-                  if(!e){
-                    res.redirect("/" + foundUser._id + "/cart")
-                  }else{console.log(err);  }
+  User.findOne({
+    email: useremail
+  }, function(e, foundUser) {
+    if (!e) {
+      if (foundUser) {
+        if (foundUser.password === userpassword) {
+          if (soupId != null) {
+            /**/
+            /*MUST DELETE*/
+            Soup.findOne({
+              _id: soupId
+            }, function(e, foundSoup) {
+              if (foundSoup) {
+                console.log(foundUser.order);
+                console.log(foundUser.order.id);
+                //BEFORE UPDATING CHECK IF THE SOUP IS NOT IN THE CART!
+                let soupInTheCart = false
+                foundUser.order.forEach(function(soup, iterator) {
+                  if (foundSoup.title === soup.title  ) {
+                    //The soup the user wants is in his cart
+                    soupInTheCart = true;
+                  }
                 })
-              }else{console.log(e);
+                console.log(foundUser.order.length);
+
+                console.log(soupInTheCart)
+                if (soupInTheCart != true) {
+                  //Soup added to cart is not in the user cart
+
+                  User.updateOne({
+                    _id: foundUser._id
+                  }, {
+                    $push: {
+                      order: foundSoup
+                    }
+                  }, function(e) {
+                    if (!e) {
+                      console.log(foundUser.order);
+                      console.log(foundUser.order.id);
+                      res.redirect("/" + foundUser._id + "/cart")
+                    } else {
+                      console.log(err);
+                    }
+                  })
+                } else {
+                  //Soup Added to cart is actually in there
+                  res.redirect("/" + foundUser._id + "/cart")
+                }
+              } else {
+                console.log(e);
               }
             })
-          }else{
-          res.redirect("/" + foundUser._id + "/cart")
+          } else {
+            res.redirect("/" + foundUser._id + "/cart")
           }
-        }else{
+        } else {
           console.log("Contraseña incorrecta");
         }
-      }else{
+      } else {
         res.redirect("/register")
       }
+    } else {
+
     }
-    else{e}
   })
 })
-app.get("/register",function(req,res){
+
+app.get("/register", function(req, res) {
   res.render("register");
 })
-app.post("/register",function(req,res){
+app.post("/register", function(req, res) {
   const newUser = new User({
-    name : req.body.firstName,
-    lastName : req.body.lastName,
-    address : req.body.address,
-    email :  req.body.email,
-    password : req.body.password
+    name: req.body.firstName,
+    lastName: req.body.lastName,
+    address: req.body.address,
+    email: req.body.email,
+    password: req.body.password
   })
-  newUser.save(function(e,createdUser){
-    if(!e){
+  newUser.save(function(e, createdUser) {
+    if (!e) {
       res.redirect("/" + createdUser._id + "/cart")
-    }else{
+    } else {
       console.log(e);
     }
   })
 })
-app.get("/:id/cart",function(req,res){
-  User.findOne({_id : req.params.id},function(e,foundUser){
-    if(!e){
-      if(foundUser){
-        res.render("cart",{
-          soups : foundUser.order
+app.get("/:id/cart", function(req, res) {
+  User.findOne({
+    _id: req.params.id
+  }, function(e, foundUser) {
+    if (!e) {
+      if (foundUser) {
+        res.render("cart", {
+          soups: foundUser.order
         });
-      }else{
+      } else {
 
       }
     }
@@ -147,27 +196,29 @@ app.get("/:id/cart",function(req,res){
 
 });
 
-app.get("/soups/:id",function(req,res){
+app.get("/soups/:id", function(req, res) {
   const soupID = req.params.id;
-  Soup.findOne({_id : soupID},function(e,foundSoup){
-    res.render("fooddetails",{
-      soup : foundSoup
+  Soup.findOne({
+    _id: soupID
+  }, function(e, foundSoup) {
+    res.render("fooddetails", {
+      soup: foundSoup
     });
   })
 })
-app.post("/soups/:id",function(req,res){
+app.post("/soups/:id", function(req, res) {
   const id = req.params.id;
-  res.render("login",{
-    id : id
+  res.render("login", {
+    id: id
   })
 })
 
 
-app.get("/signin/:id",function(req,res){
+app.get("/signin/:id", function(req, res) {
   const id = req.params.id;
   res.render("signin")
 })
 
-app.listen(8080,function(){
+app.listen(8080, function() {
   console.log("Page running on port 8080");
 })
--- public/javascript/navbar.js
+++ public/javascript/navbar.js
@@ -1,17 +1,77 @@
 var quantity = 1;
-$(function () {
-  $(document).scroll(function () {
+$(function() {
+  $(document).scroll(function() {
     $(".navbar").toggleClass('scrolled', $(this).scrollTop() > $(".navbar").height());
     $(".navbar-brand").toggleClass('navbar-brand-white', $(this).scrollTop() > $(".navbar").height());
   });
 });
-$(".userSoupBtnMin").on("click",function(){
-  if(quantity > 1){
-      quantity--;
-    $(".userSoupQuantity").text(quantity);
-  }else{}
+/*HANDLING PRODUCT Quantity*/
+
+$(".userSoupBtnMin").on("click", function() {
+  switch ($("strong")){
+    case $("strong").hasClass("0"):
+      updateQuantityNumberSubsctract(parseInt($(".0").text()), "0")
+
+      break;
+    case $("strong").hasClass("1"):
+      updateQuantityNumberSubsctract(parseInt($(".1").text()), "1")
+
+      break;
+    case $("strong").hasClass("2"):
+      updateQuantityNumberSubsctract(parseInt($(".2").text()), "2")
+
+      break;
+    case $("strong").hasClass("3"):
+      updateQuantityNumberSubsctract(parseInt($(".3").text()), "3")
+
+      break;
+    case $("strong").hasClass("4"):
+      updateQuantityNumberSubsctract(parseInt($(".4").text()), "4")
+
+      break;
+    default:
+  }
 });
-$(".userSoupBtnMax").on("click",function(){
-  quantity++;
-  $(".userSoupQuantity").text(quantity);
+
+function updateQuantityNumberSubsctract(currentQuantity, soupPosition) {
+  if (currentQuantity > 1) {
+    currentQuantity--;
+    $(("." + soupPosition)).text(currentQuantity);
+  } else {}
+}
+function updateQuantityNumberAdd(currentQuantity,soupPosition){
+  currentQuantity++;
+  $(("." + soupPosition)).text(currentQuantity);
+}
+
+$(".userSoupBtnMax").on("click", function() {
+  switch ($("strong")) {
+    case $("strong").hasClass("0"):
+      console.log("HOLA MUNDO")
+      updateQuantityNumberAdd(parseInt($(".1").text()), "0")
+
+      break;
+    case $("strong").hasClass("1"):
+    console.log("HOLA MUNDO2")
+      updateQuantityNumberAdd(parseInt($(".1").text()), "1")
+
+      break;
+    case $("strong").hasClass("2"):
+    console.log("HOLA MUNDO3")
+      updateQuantityNumberAdd(parseInt($(".2").text()), "2")
+
+      break;
+    case $("strong").hasClass("3"):
+    console.log("HOLA MUNDO4")
+      updateQuantityNumberAdd(parseInt($(".3").text()), "3")
+
+      break;
+    case $("strong").hasClass("4"):
+    console.log("HOLA MUNDO5")
+      updateQuantityNumberAdd(parseInt($(".4").text()), "4")
+
+      break;
+    default:
+  }
+
 });
